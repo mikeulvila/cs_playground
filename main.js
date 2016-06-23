84801202e8bd180e5a9ed2b2a89ad08e09bba6aa
Mike Ulvila <mikeulvila@me.com>
2016-06-22 20:31:09 -0500

    bubbleSort and selectionSort functions

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var unsorted = [3,6,7,2,1,5,8,4];
+
 function swap (items, item1, item2) {
   var temp = items[item1];
   items[item1] = items[item2];
   items[item2] = temp;
-}
+};
 
 function selectionSort(array) {
   var length = array.length;
@@ -11,11 +13,31 @@ function selectionSort(array) {
   for (i = 0; i < length; i++) {
     min = i;
     for (j = i+1; j < length; j++) {
-      if ()
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      swap(array, i, min);
     }
   }
+  return array;
+};
+
+function bubbleSort(array) {
+  var length = array.length;
+  var i, j;
+
+  for (i = 0; i < length; i++) {
+    for (j = 0; j < length - i; j++) {
+      if(array[j] > array[j+1]) {
+        swap(array, j, j+1);
+      }
+    }
+  }
+  return array;
 }
 
-var unsorted = [3,6,7,2,1,5,8,4];
 
+console.log(selectionSort(unsorted));
 console.log(bubbleSort(unsorted));
