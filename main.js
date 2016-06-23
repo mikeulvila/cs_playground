d9d43053ea8260a7aaca46aa33b1bea7dcf4156d
Mike Ulvila <mikeulvila@me.com>
2016-06-22 20:08:50 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function swap (items, item1, item2) {
+  var temp = items[item1];
+  items[item1] = items[item2];
+  items[item2] = temp;
+}
+
+function selectionSort(array) {
+  var length = array.length;
+  var i, j, min;
+
+  for (i = 0; i < length; i++) {
+    min = i;
+    for (j = i+1; j < length; j++) {
+      if ()
+    }
+  }
+}
+
+var unsorted = [3,6,7,2,1,5,8,4];
+
+console.log(bubbleSort(unsorted));
